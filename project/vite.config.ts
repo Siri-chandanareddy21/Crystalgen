1d4a6429dca2fe4d382123f84f8cca6087926deb
Siri-chandanareddy21 <sirichandanareddynukala@gmail.com>
2025-11-26 01:19:31 +0530

    Add Jenkins pipeline configuration to vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -8,3 +8,34 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
 });
+////////////
+pipeline {
+    agent any
+    tools{
+        maven 'MAVEN_HOME'
+    }
+    stages {
+        stage('git repo & clean') {
+            steps {
+                //bat "rmdir  /s /q mavenjava"
+                bat "git clone https://github.com/Harshinampally/Hibernate.git"
+                bat "mvn clean -f Hibernate"
+            }
+        }
+        stage('install') {
+            steps {
+                bat "mvn install -f Hibernate" 
+            }
+        }
+        stage('test') {
+            steps {
+                bat "mvn test -f Hibernate"
+            }
+        }
+        stage('package') {
+            steps {
+                bat "mvn package -f Hibernate"
+            }
+        }
+    }
+}\
